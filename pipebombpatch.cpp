8030ba2bef0bef0d4f479981fcdd70c4ea2ff1b5
infernalxdrm <36190565+infernalxdrm@users.noreply.github.com>
2021-04-27 20:11:35 +0700

    im so done

--- pipebombpatch.cpp
+++ pipebombpatch.cpp
@@ -40,7 +40,7 @@ struct fakeGlobals {
 
 fakeGlobals g_FakeGlobals = { {0.0, 0.0, 0.0, 0.0}, 0.015151515 };
 fakeGlobals *gp_FakeGlobals = &g_FakeGlobals;
-static int PipeBombFrameTimePatch::FrameTimeNumCounter = 0;
+int PipeBombFrameTimePatch::FrameTimeNumCounter = 0;
 
 PipeBombFrameTimePatch::PipeBombFrameTimePatch(IServerGameDLL * gamedll)
 {
